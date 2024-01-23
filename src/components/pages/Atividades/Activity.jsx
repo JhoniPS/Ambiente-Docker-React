56d063999626e75e9bb38961f4308d908f4cfb60
jhonisilva <jhonisilva500@gmail>
2024-01-23 16:30:25 -0300

    criação da tela de atividades

--- src/components/pages/Atividades/Activity.jsx
+++ src/components/pages/Atividades/Activity.jsx
@@ -1,6 +1,6 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react';
 import MenuAppBar from '../../layout/AppBar/MenuAppBar';
-import { CButton, CCard, CCardBody, CCol, CContainer, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle, CFormInput, CInputGroup, CInputGroupText, CRow } from '@coreui/react';
+import { CButton, CCard, CCardBody, CCollapse, CContainer, CFormCheck, CFormInput, CInputGroup, CInputGroupText, CRow } from '@coreui/react';
 import CIcon from '@coreui/icons-react';
 import { cilBook } from '@coreui/icons';
 import api from '../../../services/api';
@@ -9,12 +9,14 @@ import { useParams } from 'react-router-dom';
 function Activity() {
     const { id } = useParams();
     const [activitys, setActivitys] = useState([]);
+    const [openCards, setOpenCards] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await api.get(`group/${id}/activity`);
                 setActivitys(response.data);
+                setOpenCards(new Array(response.data.length).fill(false));
             } catch (error) {
                 console.log(error);
             }
@@ -23,13 +25,17 @@ function Activity() {
         fetchData();
     }, [id]);
 
-    console.log(activitys);
+    const toggleCard = (index) => {
+        const updatedOpenCards = [...openCards];
+        updatedOpenCards[index] = !updatedOpenCards[index];
+        setOpenCards(updatedOpenCards);
+    };
 
     return (
         <Fragment>
             <MenuAppBar />
             <div className="d-flex flex-column p-4 gap-2 h-100">
-                <h2> Cadastrar Atividade</h2>
+                <h2>Cadastrar Atividade</h2>
                 <CCard className='container-fluid'>
                     <CCardBody>
                         <CInputGroup className="mb-3">
@@ -49,17 +55,32 @@ function Activity() {
                         </CButton>
                     </CCardBody>
                 </CCard>
-
                 <h2>Lista Atividades</h2>
-                <CCard className='container-fluid'>
-                    <CCardBody>
-                        <CContainer>
-                            <CRow>
+                <CCard className='container-fluid overflow-auto' style={{ maxHeight: '500px' }}>
+                    <CCardBody className='p-0 mt-3 mb-3'>
+                        <CContainer fluid>
+                            <CRow className="d-flex p-0 flex-wrap gap-3">
                                 {activitys.length !== 0 ? (
                                     activitys.map((activity, index) => (
-                                      <CCard>
-                                        <CCardBody>Teste</CCardBody>
-                                      </CCard>
+                                        <CCard key={index} onClick={() => toggleCard(index)}>
+                                            <CCardBody className='d-flex justify-content-between'>
+                                                {activity.name}
+                                                <div className='d-flex gap-3'>
+                                                    <CFormCheck onClick={(e) => e.stopPropagation()} />
+                                                    <CFormCheck onClick={(e) => e.stopPropagation()} />
+                                                    <CFormCheck onClick={(e) => e.stopPropagation()} />
+                                                </div>
+                                            </CCardBody>
+                                            <CCollapse visible={openCards[index]} className='mb-3'>
+                                                <CCard className="mt-3">
+                                                    <CCardBody>
+                                                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                                                        squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+                                                        sapiente ea proident.
+                                                    </CCardBody>
+                                                </CCard>
+                                            </CCollapse>
+                                        </CCard>
                                     ))
                                 ) : null}
                             </CRow>
@@ -68,7 +89,7 @@ function Activity() {
                 </CCard>
             </div>
         </Fragment>
-    )
+    );
 }
 
 export default Activity;
